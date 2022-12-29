6d04feacbc80c75522c5134614de541f0dacc500
Sudeep Paul <sudeeprnp@gmail.com>
2022-12-29 12:20:24 +0530

    imported pet-shop box structure in the project

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,29 +1,27 @@
-pragma solidity 0.5.16;
+pragma solidity ^0.5.16;
 
 contract Election {
-    // Model a Candidate 
-
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
 
-    // Read/write Candidates
+    // Read/write candidates
     mapping(uint => Candidate) public candidates;
 
-     // Store Candidates Count
-    uint public candidatesCount = 0;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
 
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    constructor() public {                 
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");    
-    }
-
-    // ...
 }
